cdbfe1031fdbf3425aea59cba217cede5bbef6ec
zhyyfaan <2961695289@qq.com>
2021-11-15 19:42:54 +0800

    完整

--- src/App.vue
+++ src/App.vue
@@ -1,45 +1,43 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <router-view></router-view>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-// import axios from 'axios'
-import jsonp from 'jsonp'
-
 export default {
-  name: 'App',
+  name: 'app',
   components: {
-    HelloWorld
+    
   },
   data(){
     return {
-      age:45
+      
     }
   },
   mounted(){
-    let url = "/api/activity/servicetime";
-    // axios.get(url).then(()=>{
-
-    // })
-    jsonp(url,(err,res)=>{  //不是请求，只是一个js函数(请求不允许跨域访问的网站，用axios会报错，jsonp不会),参数为(url请求地址，(错误信息，返回结果))
-      let result = res;
-      this.data=result;
-    })
+    if(this.$cookie.get('userId')){
+      this.getUser();
+      this.getCartCount();
+    }
+  },
+  methods:{
+    getUser(){
+      this.axios.get('/user').then((res={})=>{
+        this.$store.dispatch('saveUserName',res.username);
+      })
+    },
+    getCartCount(){
+      this.axios.get('/carts/products/sum').then((res=0)=>{
+        this.$store.dispatch('saveCartCount',res);
+      })
+    }
   }
 }
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;  
-}
+<style lang="scss">
+@import './assets/scss/reset.scss';
+@import './assets/scss/config.scss';
+@import './assets/scss/button.scss';
 </style>
--- src/vue.config.js
+++ src/vue.config.js
@@ -3,14 +3,14 @@ module.exports = {
         host:'localhost',
         port:8082,
         proxy:{
-            '/api':{
+            '/api':{ //在真实url的http://www.imooc.com后面加一个虚拟的端口/api/，然后真正请求的时候把/api置空，再把后面的目标url拼上去，这样就减少了重复代码，否则对于每个端口都要重写该方法
                  /* 目标代理服务器地址 */
                 target:'http://www.imooc.com',
                 /* //changeOrigin是false：
                  请求头中host仍然是浏览器发送过来的host；
                  如果设置成true：发送请求头中host会设置成target。*/
                 changeOrigin:true,
-                pathRewrite:{//将api替换为空
+                pathRewrite:{//将api替换为空，实际转发的时候为http://www.imooc.com/activity/servicetime
                     'api':''
                 }
             }
